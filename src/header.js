e205d1676bd3af3b258ee8ab44ad71959c6ec5a4
mwbrennan <mwb5@me.com>
2021-01-21 17:56:08 -0600

    day 2

--- src/header.js
+++ src/header.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
 
 export default function Header(props) {
 
     return(
        <div>
-           <h1>{props.title} </h1>
+           <H1Style>{props.title}</H1Style>
        </div>
     )
 
 }
+
+const H1Style = styled.h1`
+    color: yellow;
+    background-color: lightcoral;
+    padding: 2rem;
+    /* padding-bottom: .25rem; */
+    margin-top: -.5rem;
+    `;
--- src/nasaPic.js
+++ src/nasaPic.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import Header from './header'
+import Header from './header';
 import axios from 'axios';
+import styled from 'styled-components';
 
 export default function Pic(props) {
 
@@ -13,7 +14,7 @@ export default function Pic(props) {
         axios
         .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
         .then(res => {
-            // console.log(res);
+            console.log(res);
             setDate(res.data.date);
             setExplanation(res.data.explanation);
             setTitle(res.data.title);
@@ -22,13 +23,64 @@ export default function Pic(props) {
         .catch(err => console.log(err))
     }, [])
 
+
+
     return(
        <div>
            <Header title={title} />
-           <h2>Photo of the day: {date}</h2>
-           <img src={url} alt="nasa picture"/>
-           <p> {explanation} </p>
+
+           <DivStyle1>
+            <h2>Photo of the day: {date}</h2>
+            <img src={url} alt="nasa picture"/>
+           </DivStyle1>
+           
+           <DivStyle2>
+             <p> {explanation} </p>
+           </DivStyle2>
+
        </div>
     )
 
 }
+
+
+
+const DivStyle1 = styled.div`
+    background-color: pink;
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    height: 100%;
+    margin-left: 2%;
+
+    h2 {
+        color: darkgray;
+        justify-content: center;
+        display: flex;
+        font-size: 2rem;
+        
+    }
+
+    img {
+        display: flex;
+        justify-content: center;
+        border-radius: 100%;
+        max-width: 100%;
+        /* width: 75%; */
+    }
+
+`;
+
+const DivStyle2 = styled.div`
+    background-color: teal;
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+
+    p {
+        color: red;
+        display: flex;
+
+    }
+
+`;
